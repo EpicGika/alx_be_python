00b76a9c576221bd0a4be783a023d51dc6f4f7b3
Taher Sanad <taher.sanad2000@gmail.com>
2024-09-13 21:22:32 +0300

    Calculating reactangle

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,8 @@
+# length = int(input("what is the rectangle length"))  #To get the length
+# width = int(input("what is the rectangle width "))   #To get the length
+# print("Rectangle is ", length * width)    #give the recatangle
+
+length = 10
+width = 5
+
+print("The area of the rectangle is:", length * width)
