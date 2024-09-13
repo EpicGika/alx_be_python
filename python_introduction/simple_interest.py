0e76d9151389c548556f39c24524d65ebc594355
Taher Sanad <taher.sanad2000@gmail.com>
2024-09-13 21:13:22 +0300

    Interest file

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000  # Reprsntig 1000$
+rate = 0.05  # Representing 5% annual interest rate
+time = 3  # Representing 3 years
+
+interest = principal * rate * time
+
+print("The simple interest is:", interest)
