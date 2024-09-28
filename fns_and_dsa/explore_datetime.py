a5567924a89020a5d575e08c695b485245fde9e6
EpicGika <Taher.sanad2000@gmail.com>
2024-09-29 00:59:58 +0300

    new file

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,41 +1,28 @@
 # explore_datetime.py
 
-import datetime
-from datetime import timedelta  # Import timedelta to calculate future dates
-
-# Part 1: Display the Current Date and Time
+from datetime import datetime, timedelta
 
 
 def display_current_datetime():
     # Get the current date and time
-    current_date = datetime.datetime.now()
-
-    # Format the date and time as "YYYY-MM-DD HH:MM:SS"
-    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
-
-    # Print the formatted date and time
-    print("Current Date and Time:", formatted_date)
-
-# Part 2: Calculate a Future Date
+    current_date = datetime.now()
+    # Format and print the current date and time
+    print("Current date and time:", current_date.strftime("%Y-%m-%d %H:%M:%S"))
 
 
 def calculate_future_date():
-    # Prompt the user to enter the number of days
-    number_of_days = int(input("Enter the number of days to add to the current date"))
-
-    # Get the current date
-    current_date = datetime.datetime.now()
+    # Prompt the user to enter a number of days
+    days_to_add = int(
+        input("Enter the number of days to add to the current date: "))
 
     # Calculate the future date
-    future_date = current_date + timedelta(days=number_of_days)
-
-    # Format the future date as "YYYY-MM-DD"
-    formatted_future_date = future_date.strftime("%Y-%m-%d")
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=days_to_add)
 
     # Print the future date
-    print("Future Date:", formatted_future_date)
+    print("Future date:", future_date.strftime("%Y-%m-%d"))
 
 
-# Call the functions
-display_current_datetime()
-calculate_future_date()
+if __name__ == "__main__":
+    display_current_datetime()
+    calculate_future_date()
